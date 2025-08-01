d4ee895f8e3bdae17ddded12def69eff5f7217ef
tanushreemadaan <tanushreemadaan1553@gmail.com>
2025-08-02 00:50:46 +0530

    Updates Cors origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.CLIENT_URL, 
+    origin: ['https://medi-queue-alpha.vercel.app', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
